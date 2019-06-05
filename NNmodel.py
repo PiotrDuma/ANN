c12d494e4ea66bff3451c6e6ff439ca206da7db5
Piotr Duma <piotrduma2014@gmail.com>
2019-06-05 11:07:33 +0200

    Added data and network classes, ploting and saving in file

--- NNmodel.py
+++ NNmodel.py
@@ -1,155 +1,198 @@
-import os,sys
+# import os,sys
 import numpy as np
 import functions
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-
-def process(x, weights, biases):
-    
-    layer1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-    layer1 = tf.nn.sigmoid(layer1)
-        
-    layer2 = tf.add(tf.matmul(layer1, weights['h2']), biases['b2'])
-    layer2 = tf.nn.sigmoid(layer2)
-    
-    outlayer = tf.matmul(layer2, weights['out']) + biases['out']
-    
-    return outlayer
-
-#START 
-mypath = "data"
-imagePath = os.path.join(sys.path[0], mypath)
-
-print("Loading images")
-# DATASET, validation = functions.preprocessing(imagePath,functions.getHOGDescriptor)
-DATASET, validation = functions.preprocessing(imagePath,functions.getHOGDescriptor)
-
-# split testing and validate data to input and output lists
-trainX = np.vstack(DATASET[0])
-trainY = np.vstack(DATASET[1])
-testX = np.vstack(validation[0])
-testY = np.vstack(validation[1])
-
-print(trainX.shape)
-print(trainY.shape)
-
-#  Parameters
-learningRate = 0.1
-epochs = 200
-batchSize = 100
-displayStep = 100
-  
-# Network Parameters
-numHidden1 = 256 # 1st layer number of neurons
-numHidden2 = 256# 2nd layer number of neurons
-
-numInput = len(DATASET[0][0]) #input shape
-numClasses = len(DATASET[1][1]) #output classes
-  
-x = tf.placeholder(tf.float32, [None, numInput])
-w = tf.Variable(tf.zeros([numInput, numClasses]))
-b = tf.Variable(tf.zeros([numClasses]))
-y = tf.placeholder(tf.float32, [None, numClasses])
-
-weights = {
-    'h1': tf.Variable(tf.truncated_normal([numInput,numHidden1])),
-    'h2': tf.Variable(tf.truncated_normal([numHidden1,numHidden2])),
-    'out': tf.Variable(tf.truncated_normal([numHidden2,numClasses]))
-    }
-
-biases = {
-    'b1': tf.Variable(tf.truncated_normal([numHidden1])),
-    'b2': tf.Variable(tf.truncated_normal([numHidden2])),
-    'out': tf.Variable(tf.truncated_normal([numClasses]))
-    }
-
-init = tf.global_variables_initializer()
-
-saver=tf.train.Saver()
-
-pred = process(x, weights, biases)
-
-# crossEntropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=pred,logits=y)
-# costFunction = tf.reduce_mean(crossEntropy)
-# costFunction = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits( logits = y, labels = pred))
-costFunction = tf.reduce_mean(-tf.reduce_sum(y*tf.log(tf.nn.softmax(pred)),1))
-
-trainingStep = tf.train.GradientDescentOptimizer(learningRate).minimize(costFunction)
-
-
-sess = tf.Session()
-sess.run(init)
-# with tf.Session() as sess:
-#     sess.run(init)
-
-error = []
-accuracyHist = []
-costHist = np.empty(shape=[1], dtype=float)
-
-for epoch in range(epochs):
-    sess.run(trainingStep, feed_dict = {x: trainX, y: trainY })
-    cost = sess.run(costFunction, feed_dict = {x: trainX, y: trainY })
-    costHist = np.append(costHist, cost)
-    
-    correctPred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
-    accuracy = tf.reduce_mean(tf.cast(correctPred,tf.float32))
-
-    predY = sess.run(pred, feed_dict={x: testX})
-    mse = tf.reduce_mean(tf.square(predY - testY))
-    
-    mseErr = sess.run(mse)
-    error.append(mseErr)
-    
-    accuracy = (sess.run(accuracy, feed_dict={x: trainX, y: trainY}))
-    accuracyHist.append(accuracy)
-    
-    print('epoch : ', epoch, ' cost: ', cost, "  error: ", mseErr, " Acc: ", accuracy)
-
-saver.save(sess, sys.path[0])
-
-plt.plot(error, 'r')
-plt.show()
+class NNmodel:
+    #  Parameters
+    learningRate = 0.1
+    epochs = 200
+#     batchSize = 100
+    displayStep = 1
+       
  
-plt.plot(accuracyHist)
-plt.show()
+    def __init__(self, hidden1=256, hidden2=256, location = "data", desc = functions.getHOGDescriptor):
+        self.numHidden1 = hidden1
+        self.numHidden2 = hidden2
+        self.numInput=0   #size of input vector
+        self.numClasses=0 #size of output (classes) vector
+        self.activeDescriptor = desc
+        
+        self.mypath = location         
+        #dataset variables
+        self.DATASET= functions.Data(self.mypath) #dataset class object
+        self.training=None
+        self.validation=None
+        self.test=None
+        
+        #Network session variables
+        self._session=None #network session
+        #network vectors: weights, biases
+        self.weights=None
+        self.biases=None
+        self._x=None
+        self._w=None
+        self._b=None
+        self._y=None
+        
+        #preinitialize numpy output tables ~ dont need to copy every epoch.
+        self.costHistory = np.zeros([self.epochs,1], dtype=float)
+        self.accuracy = np.zeros([self.epochs,1], dtype=float)
+        self.error =np.zeros([self.epochs,1], dtype=float)
+        self.classificationResults=None
+        self.classificationTable = None
+        
+    def prepareDataset(self, ref2Descriptor = None):
+        if ref2Descriptor == None:
+            ref2Descriptor = self.activeDescriptor
+            
+        self.DATASET.preprocessing(ref2Descriptor) #create Data object
+        self.training= self.DATASET.getTrainingSet()
+        self.validation= self.DATASET.getValidationSet()
+        self.test= self.DATASET.getTestingSet()
+        
+        self.numInput = len(self.training[0][0]) #input shape
+        self.numClasses = len(self.training[1][0]) #output classes
+        
+    def initNetworkVariables(self):
+        self.weights = {
+            'h1': tf.Variable(tf.truncated_normal([self.numInput,self.numHidden1])),
+            'h2': tf.Variable(tf.truncated_normal([self.numHidden1,self.numHidden2])),
+            'out': tf.Variable(tf.truncated_normal([self.numHidden2,self.numClasses]))
+            }
+        self.biases = {
+            'b1': tf.Variable(tf.truncated_normal([self.numHidden1])),
+            'b2': tf.Variable(tf.truncated_normal([self.numHidden2])),
+            'out': tf.Variable(tf.truncated_normal([self.numClasses]))
+            }
+        self._x = tf.placeholder(tf.float32, [None, self.numInput])
+        self._w = tf.Variable(tf.zeros([self.numInput,self.numClasses]))
+        self._b = tf.Variable(tf.zeros([self.numClasses]))
+        self._y = tf.placeholder(tf.float32, [None, self.numClasses])
+        
+    def process(self,x, weights, biases):
+        layer1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        layer1 = tf.nn.sigmoid(layer1)
+         
+        layer2 = tf.add(tf.matmul(layer1, weights['h2']), biases['b2'])
+        layer2 = tf.nn.sigmoid(layer2)
+     
+        outlayer = tf.matmul(layer2, weights['out']) + biases['out']
+        return outlayer
+    
+    def start(self):
+        self.initNetworkVariables()
+        init = tf.global_variables_initializer()
+        
+        #network model settings
+        pred = self.process(self._x, self.weights, self.biases) #function with placeholder
+        costFunction = tf.reduce_mean(-tf.reduce_sum(self._y*tf.log(tf.nn.softmax(pred)),1))
+        correctPred = tf.equal(tf.argmax(pred,1), tf.argmax(self._y,1))
+        accuracy = tf.reduce_mean(tf.cast(correctPred,tf.float32))
+        trainingStep = tf.train.GradientDescentOptimizer(self.learningRate).minimize(costFunction)
+        ###
+        
+        self._session = tf.Session()
+        self._session.run(init)
+      
+        for epoch in range(self.epochs):
+            #training part
+            self.training = self.shuffleNumpyXY(self.training)
+            feedDict = {self._x: self.training[0], self._y: self.training[1] }
+            self._session.run(trainingStep, feed_dict = feedDict)
+            self.costHistory[epoch] = self._session.run(costFunction, feed_dict =feedDict)
 
-# print(accuracyHist)
+            predY = self._session.run(pred, feed_dict={self._x: self.training[0]})
+            mse = tf.reduce_mean(tf.square(predY - self.training[1]))
+            
+            self.error[epoch] = self._session.run(mse)
+            #validation part
+            self.accuracy[epoch] = (self._session.run(accuracy, feed_dict={self._x: np.vstack(self.validation[0]), self._y: np.vstack(self.validation[1])}))
+            if epoch % self.displayStep==0:
+                print('epoch : ', epoch, ' cost: ', self.costHistory[epoch], "  error: ", self.error[epoch], " Acc: ", self.accuracy[epoch])
+        
+    def plot(self):
+        plt.plot(self.error, 'r')
+        plt.show()
+        
+        plt.plot(self.costHistory, 'g')
+        plt.show()
+           
+        plt.plot(self.accuracy,'b')
+        plt.show()
+        
+    def shuffleNumpyXY(self, table):
+        assert(len(table[0]) == len(table[1]))
+        p = np.random.permutation(len(table[1]))   
+        return [table[0][p], table[1][p]]
+        
+    def runTest(self):
+        self.classificationResults= np.zeros(len(self.test[1][0]),dtype=np.int16)
+        self.classificationTable = np.zeros([len(self.test[1][0]),len(self.test[1][0])],dtype=np.int16) #YxY table, [class, classified as]
+        
+        ##calculate propagation
+        pred = self.process(self._x, self.weights, self.biases) #placeholder
+        resultsX = self._session.run(pred, feed_dict={self._x: self.test[0]})
+        
+        for i in range(len(resultsX)):
+            if(np.argmax(resultsX[i]) == np.argmax(self.test[1][i])): #predict == Y
+                self.classificationResults[np.argmax(self.test[1][i])] +=1
+        
+            self.classificationTable[np.argmax(self.test[1][i])][np.argmax(resultsX[i])]+=1 ## [realY][predictedY] +=1
+          
+        print("TEST DONE.")
+        print(self.DATASET.getUniqueLabels())
+        ##check correctness
+        for i in range(len( self.classificationResults)):
+            if(self.classificationResults[i]!=self.classificationTable[i][i]):
+                print("TABLE CLASSIFICATION ERROR")
+                print(self.classificationResults)
+                print("table")
+                print(self.classificationTable)
+                
+        print("ACCURACY RESULTS: ",self.classificationResults)
+        print("ACCURACY TABLE")
+        print(self.classificationTable)
+        
+        classify = np.multiply(self.classificationResults, 1/(len(resultsX)/len(self.test[1][0])))
+        print("%%: ", classify)  
+        
+    def saveTestSeries(self):
+
+        tmp = "\nMLP " +self.activeDescriptor.__name__+" "+self.numHidden1.__str__()+", "+self.numHidden2.__str__()+" "+self.mypath+" "+"\n" 
+        with open("OUTPUT.txt", 'a') as file:
+            file.write(tmp)
+
+            file.write("costHistory:\n")
+            np.savetxt(file,self.costHistory, newline=',')
+
+            file.write("\naccuracy:\n")
+            np.savetxt(file,self.accuracy, newline=',')
+
+            file.write("\nError:\n")
+            np.savetxt(file,self.error, newline=',')
+            file.write("\n" )
+
+            for word in self.DATASET.getUniqueLabels():
+                file.write(word + ', ')
+            file.write("\nResults:\n" )
+            for word in self.classificationResults:
+                file.write(word.__str__() + ', ')
+            file.write("\nTABLE\n" )
+            np.savetxt(file,self.classificationTable,fmt = '%5i')
+            file.write("\n* * * * * * * * * * * * * * * *\n" )
+            
 
 
-####CLASSIFICATION RESULTS
-
-classificationResults = np.zeros(len(testY[0]))
-print("TESTS")
-
-##calculate propagation
-resultsX = sess.run(pred, feed_dict={x: testX})
-
-for i in range(len(resultsX)):
-    if(np.argmax(resultsX[i]) == np.argmax(testY[i])):
-        classificationResults[np.argmax(testY[i])] +=1
-
-
-print("SHAPE: ",classificationResults)
-
-classify = np.multiply(classificationResults, 1/(len(resultsX)/len(testY[0])))
-
-
-names = sorted(os.walk(imagePath).__next__()[1])
-# print(names)
-
-tmp = np.arange(len(names))
-plt.bar(tmp, classify)
-plt.xticks(tmp, names,rotation=45)
-plt.show()
-
-# 
-# with open("error_histogram.txt", 'w') as f:
-#     for s in error:
-#         f.write(str(s) + '\n')
-# 
-
-
-np.savetxt('acc_hist_2.txt', accuracyHist, delimiter='\n')   # X is an array
-
-
+if __name__ == "__main__":
+    myDescriptors =[functions.getHOGDescriptor ]
+    mynetwork = NNmodel(64,64, "data2",myDescriptors[0])
+    mynetwork.prepareDataset()
+    print("training set length: ",len(mynetwork.training[0]))
+    print("validation set length: ",len(mynetwork.validation[0]))
+    print("testing set length: ",len(mynetwork.test[0]))
+    mynetwork.start()
+    mynetwork.plot()
+    mynetwork.runTest()
+    mynetwork.saveTestSeries()
--- functions.py
+++ functions.py
@@ -2,117 +2,128 @@ import cv2
 import os
 import re
 import numpy as np
-import tensorflow as tf
 import random
 import mahotas
 
 
+class Data:
+    def __init__(self, location = "data"):
+        self.path = os.path.join(os.path.dirname(__file__), location)
+        self.uniqueLabels=None
+        self.trainingSet=None
+        self.validationSet=None
+        self.testingSet=None
+        
+        
+    def preprocessing(self,functionDescriptor1):
+
+        filenameTraining, labelTraining, filenameValidate, labelValidate =  self._getLearningImages()
+        filenameTesting, labelTesting = self._getTestingImages()
+        
+        #load training data
+        self.trainingSet = self._getImageData(filenameTraining, labelTraining, functionDescriptor1)
+        
+        #load validate data
+        self.validationSet= self._getImageData(filenameValidate, labelValidate, functionDescriptor1)
+        
+        #load test data
+        self.testingSet = self._getImageData(filenameTesting, labelTesting, functionDescriptor1)
+        print("Loading Data Completed")
+
+    ## return array of [x,y] which are numpy arrays
+    def getTrainingSet(self):
+        return [np.vstack(self.trainingSet[0]),np.vstack(self.trainingSet[1])]
+    def getValidationSet(self):
+        return [np.vstack(self.validationSet[0]),np.vstack(self.validationSet[1])]
+    def getTestingSet(self):
+        return [np.vstack(self.testingSet[0]),np.vstack(self.testingSet[1])]
+    def getUniqueLabels(self):
+        return self.uniqueLabels
+    
+    def _loadImage(self,imgPath):
+        img = cv2.imread(imgPath,cv2.IMREAD_UNCHANGED)
+        img = cv2.resize(img, (100, 100)) 
+        return img
+    
+    #return tables of images' paths and their labels
+    def _getLearningImages(self):
+        imgPath = os.path.join(self.path, "learn")
+        classes = sorted(os.walk(imgPath).__next__()[1])
+        self.uniqueLabels = classes
+    
+        trainingSetX= []
+        trainingSetY= []
+        validationSetX = []
+        validationSetY = []
+        for c in classes:
+                imagesPath, imagesLabel = list(), list()
+                c_dir = os.path.join(imgPath, c)
+                walk = os.walk(c_dir).__next__()
+                label = ([0])*len(classes)
+                label[classes.index(c)]=1.0
+                for sample in walk[2]:
+                    if sample.endswith('.jpg') or sample.endswith('.jpeg') or sample.endswith('.png'):
+                        imagesPath.append(os.path.join(c_dir, sample))
+                        imagesLabel.append(label)
+                        
+                Lx, Ly, Vx,Vy = self._splitData(imagesPath, imagesLabel)
+                trainingSetX.extend(Lx)
+                trainingSetY.extend(Ly)
+                validationSetX.extend(Vx)
+                validationSetY.extend(Vy)         
+        return trainingSetX, trainingSetY, validationSetX, validationSetY
+    
+    def _getTestingImages(self):
+        imgPath = os.path.join(self.path, "test")
+        classes = sorted(os.walk(imgPath).__next__()[1])
+        if(classes != self.uniqueLabels):
+            print("test and learn classes are miscellaneous!")
+        classes = sorted(os.walk(imgPath).__next__()[1])
+        imagesPath, imagesLabel = list(), list()
+        for c in classes:   
+                c_dir = os.path.join(imgPath, c)
+                walk = os.walk(c_dir).__next__()
+                label = ([0])*len(classes)
+                label[classes.index(c)]=1.0
+                for sample in walk[2]:
+                    if sample.endswith('.jpg') or sample.endswith('.jpeg') or sample.endswith('.png'):
+                        imagesPath.append(os.path.join(c_dir, sample))
+                        imagesLabel.append(label)                         
+        return imagesPath, imagesLabel
+        
+    #return shuffled numpy table of 2 lists X,Y, of features and labels
+    def _getImageData(self,filenames, labels, functionDescriptor):
+        features= []
+        for c in filenames:
+            features.append(functionDescriptor(c))
+            
+        features,label = self._shuffleData(features,labels)
+        dataset = np.array([features,label])
+        return dataset
+    
+    def _shuffleData(self, tableX, tableY):
+        data = list(zip(tableX,tableY))
+        random.shuffle(data)
+        x, y = list(zip(*data))
+        return x,y
+    
+        #slit 80% data for learning and 20 for testing
+    def _splitData(self,images, labels):
+        x, y = self._shuffleData(images, labels)   
+        learnX = x[0:int(0.8*len(images))]
+        learnY = y[0:int(0.8*len(images))]
+        valX = x[int(0.8*len(images)):]
+        valY = y[int(0.8*len(images)):]
+        return learnX, learnY, valX, valY
+
+
+############################
 def loadImage(imgPath):
     img = cv2.imread(imgPath,cv2.IMREAD_UNCHANGED)
-#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = cv2.resize(img, (100, 100)) 
+    return img    
+ 
 
-    img = cv2.resize(img, (100, 100))
-
-    return img
-    
-
-def getImages(imgPath):
-    classes = sorted(os.walk(imgPath).__next__()[1])
-#     imagesPath, imagesLabel = list(), list()
-    
-#     print(classes)
-#     for c in classes:
-#             c_dir = os.path.join(imgPath, c)
-#             walk = os.walk(c_dir).__next__()
-#             label = ([0])*len(classes)
-#             label[classes.index(c)]=1.0
-#             for sample in walk[2]:
-#                 if sample.endswith('.jpg') or sample.endswith('.jpeg'):
-#                     imagesPath.append(os.path.join(c_dir, sample))
-#                     imagesLabel.append(label)
-#     return imagesPath, imagesLabel
-        
-    trainingSetX= []
-    trainingSetY= []
-    validationSetX = []
-    validationSetY = []
-    for c in classes:
-            imagesPath, imagesLabel = list(), list()
-            c_dir = os.path.join(imgPath, c)
-            walk = os.walk(c_dir).__next__()
-            label = ([0])*len(classes)
-            label[classes.index(c)]=1.0
-            for sample in walk[2]:
-                if sample.endswith('.jpg') or sample.endswith('.jpeg') or sample.endswith('.png'):
-                    imagesPath.append(os.path.join(c_dir, sample))
-                    imagesLabel.append(label)
-                    
-            Lx, Ly, Vx,Vy = splitData(imagesPath, imagesLabel)
-            trainingSetX.extend(Lx)
-            trainingSetY.extend(Ly)
-            validationSetX.extend(Vx)
-            validationSetY.extend(Vy)         
-    return trainingSetX, trainingSetY, validationSetX, validationSetY
-
-#slit 80% data for learning and 20 for testing
-def splitData(images, labels):
-    data = list(zip(images,labels))
-    random.shuffle(data)
-
-    x, y = list(zip(*data))
-    
-    learnX = x[0:int(0.8*len(images))]
-    learnY = y[0:int(0.8*len(images))]
-    
-    valX = x[int(0.8*len(images)):]
-    valY = y[int(0.8*len(images)):]
-    return learnX, learnY, valX, valY
-
-def preprocessing(imgPath, functionDescriptor1):
-    filenameTraining, labelTraining, filenameValidate, labelValidate =  getImages(imgPath)
-
-    #load training data
-    trainingDataset = getData(filenameTraining, labelTraining, functionDescriptor1)
-    
-    #load test data
-    validateDataset = getData(filenameValidate, labelValidate, functionDescriptor1)
-     
-    return trainingDataset,validateDataset
-
-#descriptor as image for CNN
-def getDescriptor(imgPath):
-    filename, label = getImages(imgPath)
-    dataset = tf.data.Dataset.from_tensor_slices((filename, label))
-    
-    def local(filename, label):
-        image_string = tf.read_file(filename)
-        image_decoded = tf.image.decode_jpeg(image_string, channels=3)
-        image = tf.cast(image_decoded, tf.float32)
-        return image, label
-    dataset = dataset.map(local)
-    return dataset
-
-
-def getData(filename, label, functionDescriptor):
-    features= []
-
-    for c in filename:
-        features.append(functionDescriptor(c))
-        
-#     features= np.array(features)
-#     label=np.array(label)
-#     dFeatures = tf.data.Dataset.from_tensor_slices(features)
-#     dLabels = tf.data.Dataset.from_tensor_slices(label)
-#     dataset = tf.data.Dataset.zip((dFeatures,dLabels)).shuffle(500).batch(50)
-
-    dataset = list(zip(features,label))
-    random.shuffle(dataset)
-    features,label = list(zip(*dataset))
-
-    dataset = np.array([features,label])
-
-    return dataset
     
 #############################################################
 ###################     DESCRIPTORS   #######################
